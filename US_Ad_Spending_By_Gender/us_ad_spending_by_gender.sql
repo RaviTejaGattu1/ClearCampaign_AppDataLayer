e5621670b02f0e351a2175d83125c01b6241ba92
JacobCulaton <jacob.culaton@sjsu.edu>
2024-04-27 13:18:45 -0700

    JOIN query update

--- US_Ad_Spending_By_Gender/us_ad_spending_by_gender.sql
+++ US_Ad_Spending_By_Gender/us_ad_spending_by_gender.sql
@@ -1,11 +1,13 @@
 SELECT
-  cs.gender_targeting, COUNT(*) as gender_count, SUM(ags.spend_usd) AS total_usd
+  cs.gender_targeting, COUNT(DISTINCT cs.ad_id) as gender_count, SUM(ags.spend_usd) AS total_usd
 FROM
   bigquery-public-data.google_political_ads.creative_stats AS cs
 JOIN
   bigquery-public-data.google_political_ads.advertiser_geo_spend AS ags
 ON
   cs.advertiser_id = ags.advertiser_id
+WHERE
+  ags.country = 'US'
 GROUP BY
   cs.gender_targeting
 LIMIT
